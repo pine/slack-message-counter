a272c5f8fb2eaf62284728b77c291cd37727c9db
Pine Mizune <pinemz@gmail.com>
2017-02-26 09:46:21 +0900

    chore(init): initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+slack-message-counter
+---------------------
+
+Count yor Slack messages.
+
+## Requirements
+
+- Node v7.6.0 or later
+- Yarn
+
+## Getting started
+
+```
+$ yarn
+$ SLACK_API_TOKEN=your_slack_token yarn start
+```
+
+## License
+Public Domain
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const Promise = require('bluebird')
+const promiseRetry = require('promise-retry')
+const { WebClient } = require('@slack/client')
+const { sprintf } = require('sprintf-js')
+
+// ----------------------------------------------------------------------------
+
+const token = process.env.SLACK_API_TOKEN
+if (!token) {
+  console.error('Slack API token required')
+  process.exit(1)
+}
+
+const web = new WebClient(token)
+Promise.promisifyAll(web.channels.__proto__)
+Promise.promisifyAll(web.groups.__proto__)
+Promise.promisifyAll(web.search.__proto__)
+
+// ----------------------------------------------------------------------------
+
+!async function () {
+  const { channels } = await web.channels.list()
+  const { groups } = await web.groups.list()
+
+  const results = []
+  await Promise.all(channels.map(({ name }) => promiseRetry(async () => {
+    const { messages: { total } } = await web.search.messages(`in:#${name}`)
+    results.push({ name, total })
+  })))
+  await Promise.all(groups.map(({ name }) => promiseRetry(async () => {
+    const { messages: { total } } = await web.search.messages(`in:${name}`)
+    results.push({ name, total })
+  })))
+
+  results.sort((a, b) =>
+    a.total > b.total ? -1 :
+    a.total < b.total ?  1 : 0
+  )
+
+  for (const result of results) {
+    console.log(sprintf('%5d %s', result.total, result.name))
+  }
+}()
